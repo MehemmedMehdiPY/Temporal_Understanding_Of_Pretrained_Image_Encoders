d3763668fc679067e78110f9287b139394c22640
Learner Learner <mehemmedmehdi2003@gmail.com>
2025-02-10 00:37:30 +0400

    ReLU removed

--- models/model.py
+++ models/model.py
@@ -9,9 +9,8 @@ class FramePredictor(nn.Module):
         self.hidden_size = hidden_size
         self.backbone = ConvLSTM(input_size=input_size, hidden_size=hidden_size, kernel_size=kernel_size, num_layers=5)
         self.conv = nn.Conv2d(in_channels=hidden_size, out_channels=input_size[0], kernel_size=kernel_size, padding="same")
-        self.relu = nn.ReLU()
-
+        
     def forward(self, x):
         o, _ = self.backbone(x)
-        o = self.relu(self.conv(o))
+        o = self.conv(o)
         return o
