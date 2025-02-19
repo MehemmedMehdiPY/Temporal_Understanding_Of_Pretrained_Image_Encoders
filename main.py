63b0af0e6c4c6fdc4b56d45db9e629c7e69b8948
Learner Learner <mehemmedmehdi2003@gmail.com>
2025-02-19 21:30:10 +0400

    save function removed

--- main.py
+++ main.py
@@ -9,19 +9,9 @@ from utils.processing import MovingMNIST
 from utils.train import Trainer
 from PIL import Image
 
-def save(out, i: int):
-    out = out[0, [0]].cpu().detach().numpy() * 255
-    out = out.repeat(3, axis=0).astype(np.uint8)
-    out = out.transpose([1, 2, 0])
-    image = Image.fromarray(out)
-    image.save("./images/image_{}.png".format(i))
-
 DEVICE = "cpu"
 
 dataset = MovingMNIST("../data")
-X, Y = dataset[0]
-print(X.shape, Y.shape)
-print(X.dtype, Y.dtype)
 
 train_loader = DataLoader(dataset, batch_size=16, shuffle=True)
 val_loader = DataLoader(dataset, batch_size=256, shuffle=False)
@@ -32,8 +22,4 @@ loss_fn = nn.BCELoss()
 trainer = Trainer(
     model=model, train_loader=train_loader, val_loader=val_loader,
     optimizer=optimizer, loss_fn=loss_fn, epochs=2, filepath="./saved_models/model.pt")
-
-for X, Y in train_loader:
-    model(X)
-    break
-# trainer.run()
+trainer.run()
